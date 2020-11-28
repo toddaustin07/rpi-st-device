cb228a8388965239f1f275bba386008e4831d130
Todd Austin <toddaustin07@yahoo.com>
2020-11-28 17:59:50 -0600

    new updates

--- initsoftapconf.sh
+++ initsoftapconf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.ORIGconf
-sudo cp ~/rpi-st-device/hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp ~/rpi-st-device/RPIhostapd.conf /etc/hostapd/hostapd.conf
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.ORIGconf
-sudo cp ~/rpi-st-device/dnsmasq.conf /etc/dnsmasq.conf
+sudo cp ~/rpi-st-device/RPIdnsmasq.conf /etc/dnsmasq.conf
--- sdkbuildsetup.sh
+++ sdkbuildsetup.sh
@@ -4,8 +4,8 @@ cd ~/st-device-sdk-c
 mv src/port/bsp/posix/iot_bsp_wifi_posix.c /src/port/bsp/posix/iot_bsp_wifi_posix.ORIGc
 cp ~/rpi-st-device/iot_bsp_wifi_rpi.c src/port/bsp/posix/iot_bsp_wifi_posix.c
 mv Makefile MakefileORIG
-cp ~/rpi-st-device/SDKMakefile Makefile
+cp ~/rpi-st-device/RPIMakefile Makefile
 mv stdkconfig stdkconfigORIG
-cp ~/rpi-st-device/stdkconfig stdkconfig
+cp ~/rpi-st-device/RPIstdkconfig stdkconfig
 
 
