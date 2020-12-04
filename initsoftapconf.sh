df8dbae9c350af029d2480caecdda6e7a09e2391
Todd Austin <toddaustin07@yahoo.com>
2020-12-04 15:34:28 -0600

    Delete initsoftapconf.sh

--- initsoftapconf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.ORIGconf
-sudo cp ~/rpi-st-device/RPIhostapd.conf /etc/hostapd/hostapd.conf
-sudo mv /etc/dnsmasq.conf /etc/dnsmasq.ORIGconf
-sudo cp ~/rpi-st-device/RPIdnsmasq.conf /etc/dnsmasq.conf
